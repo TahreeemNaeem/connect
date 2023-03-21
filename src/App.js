fda713f96e895424f8d804d3166a927ad238264a
TahreeemNaeem <128479748+TahreeemNaeem@users.noreply.github.com>
2023-03-21 21:29:44 +0500

    new

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import { InjectedConnector } from '@wagmi/core/connectors/injected'
 import { getAccount } from '@wagmi/core'
 
 
-function App() {
+ function App() {
   const account = getAccount()
   const address =account.address
+
   if(account.isConnected){
     return (
-      <div> connected to 
-      {address}
+      <div>
+        <h1>Wallet Connect</h1> <div>connected to </div>
+        {address}
       </div>
     );
   }
    return (
     <div>
+      <h1>Wallet Connect</h1>
       <button onClick={() => {connect({ connector: new InjectedConnector(), }) }}
      >Connect Wallet</button>
     </div>
