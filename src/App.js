4f1be0cc1201844f6721a512d99e9d1231a7861f
TahreeemNaeem <tahreem.naeem@duolabz.com>
2023-03-29 01:16:08 +0500

    ethers

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useAccount, useConnect, useDisconnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import Mint from './components/mint';
 import Contract from './components/contract';
-import ContractInteraction from './components/contract';
+import ContractInteraction from './components/contractInteraction';
 
 
 function App() {
